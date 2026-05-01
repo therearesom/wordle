291b6273aef398b216b8e56e7c0e01886a209605
therearesom <jernej2468c@gmail.com>
2026-05-01 19:24:27 +0200

    quickfix

--- wordle.py
+++ wordle.py
@@ -39,7 +39,8 @@ while True:
     except ValueError:
         print("Enter a valid number!")
 
-secret_word_letters = list(secret_word_gen(no_of_letters))
+secret_word = secret_word_gen(no_of_letters)
+secret_word_letters = list(secret_word)
 game_result = False
 win = ["G" for i in range(no_of_letters)]
 no_of_attempts = 0
@@ -77,4 +78,4 @@ if game_result == False:
 else:
     print(f"You won in {no_of_attempts} attempts!")
 
-print(f"The secret word was {secret_word_gen(no_of_letters)}.")
+print(f"The secret word was {secret_word}.")
